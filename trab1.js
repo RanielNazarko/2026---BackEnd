16b56b23eb097db63456fea31d264c7203c7add2
Raniel de Jesus Nazarko <raniel.nazarko@escola.pr.gov.br>
2026-03-23 08:55:54 -0300

    Add files via upload

---
+++ trab1.js
@@ -0,0 +1,7 @@
+const produtos = ["Pão", "Açucar", "Refrigerante", "Macarrão", "Chocolate", "Arroz", "Feijão", "Leite", "Pasta de Dente", "Bolacha"]
+const precos = [0.75, 5.50, 11.50, 4.50, 5.99, 32.25, 9.70, 6.80, 7.30, 5.15]
+const Q_estoque = [25, 51, 90, 26, 18, 73, 49, 67, 90, 07]
+
+function lerProduto(produto){
+
+}
