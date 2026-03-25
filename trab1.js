788ebe71b0df202b60fb9fc485cb1718684a0327
Raniel de Jesus Nazarko <raniel.nazarko@escola.pr.gov.br>
2026-03-24 23:05:26 -0300

    Update trab1.js

--- trab1.js
+++ trab1.js
@@ -2,6 +2,9 @@ const produtos = ["Pão", "Açucar", "Refrigerante", "Macarrão", "Chocolate", "
 const precos = [0.75, 5.50, 11.50, 4.50, 5.99, 32.25, 9.70, 6.80, 7.30, 5.15]
 const Q_estoque = [25, 51, 90, 26, 18, 73, 49, 67, 90, 07]
 
+     console.log("ESTOQUE")
 function lerProduto(produto){
-
+     console.log("Produto " + produto[0] " Preço: " + precos[0])
 }
+
+produtos.forEach()
